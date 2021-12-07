716b69a8a1c897a61f7390cea16dda15af96ea49
kenzostore <90239401+kenzostore@users.noreply.github.com>
2021-12-07 20:56:54 +0700

    Update index.js
    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Script by Xeon 
 // 𝐃𝐨𝐠𝐞 𝐁𝐨𝐭
-// ©Xeon Bot Incorporated
+// ©Kenz Bot Incorporated
 const {
   WAConnection: _WAConnection,
   MessageType,
@@ -139,8 +139,8 @@ nopref = true
 numbernye = '0'
 autoketik = false;
 prefixStatus = false;
-targetpc = "916909137213"; 
-owner = "916909137213"; 
+targetpc = "62882007289925"; 
+owner = "62882007289925"; 
 fakeyoi = "Xeon"; 
 HunterApi = "Ikyy69", 
 xchillds = 'XChillDs' 
@@ -150,7 +150,7 @@ zeksapi = "vallganz5",
 dapapi = "anakasu",
 ApiZeks = "https://api.zeks.xyz",
 zeksApikey = "Alphabott",
-nomorowner1 = "@916909137213"; 
+nomorowner1 = "@62882007289925"; 
 fake = `𝗢𝘄𝗻𝗲𝗿 : ${fakeyoi}\n㋛︎ 𝗣𝗿𝗲𝗳𝗶𝘅 :「 ${prefixStatus ? "Multi Prefix" : "No Prefix"} 」`;
 thumb = fs.readFileSync("./stik/thumb.jpeg"); 
 numbernye = "0"; 
